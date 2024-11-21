17e6d49b2bfd8d6499bd4b3ca0898b66b6688c35
NivashiniRamganesh30 <nivashini3093@gmail.com>
2024-11-21 13:06:16 +0530

    Add files via upload
    day2

---
+++ VariableTask.java
@@ -0,0 +1,20 @@
+public class VariableTask
+{
+public static void main(String args[]){
+byte bytevar=56;
+short shortvar=15657;
+int intvar=23654875;
+long longvar=9876543210l;
+float floatvar=157897;
+double doublevar=598745624;
+char charvar='N';
+boolean booleanvar=false;
+System.out.println("byte"+(bytevar));
+System.out.println("int"+(intvar));
+System.out.println("long"+(longvar));
+System.out.println("float"+(floatvar));
+System.out.println("double"+(doublevar));
+System.out.println("char"+(charvar));
+System.out.println("booloean"+(booleanvar));
+}
+}
