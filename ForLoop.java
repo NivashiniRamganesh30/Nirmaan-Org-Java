ede373d8da3a59cfaea0933f2542a01701c22087
NivashiniRamganesh30 <nivashini3093@gmail.com>
2024-11-26 13:18:16 +0530

    Add files via upload
    day4

---
+++ ForLoop.java
@@ -0,0 +1,20 @@
+
+	package day4;
+	import java.util.Scanner;
+	
+	public class ForLoop {
+		
+		public static void main(String [] args) {
+			Scanner na = new Scanner(System.in);
+			int table=na.nextInt();
+			for (int i=1;i<=10;i++) {
+				System.out.println(table+"x"+i+"="+i*table);
+				
+			}
+			
+			
+			
+		}
+		}
+
+	
