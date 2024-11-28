2bc62cfce8e472dc228650983592fe46866eefaa
NivashiniRamganesh30 <nivashini3093@gmail.com>
2024-11-28 13:22:57 +0530

    Add files via upload
    day6

--- Student.java
+++ Student.java
@@ -1,34 +1,31 @@
-package day7;
+package day6;
 
 public class Student {
-	String studentname="";
-    int age=0;
-    String gender="";
-    int dayspresent=0;
-    public static void main(String[] args) {
-    	
-		Student Student1= new Student();
-		Student1.studentname="Ashwin";
+	String studentName="";
+	int age=0;
+	String gender="";
+	int numofSubjects=0;
+	
+	public static void main(String[] args) {
+		Student Student1=new Student();
+		Student1.studentName="Ashwin";
 		Student1.age=20;
 		Student1.gender="Male";
-		Student1.dayspresent=156;
-		
-		System.out.println(Student1.studentname);
+		Student1.numofSubjects=5;
+		System.out.println(Student1.studentName);
 		System.out.println(Student1.age);
-		System.out.println(Student1.gender);
-		System.out.println(Student1.dayspresent);
-
-		
-		Student Student2= new Student();
-		Student2.studentname="Nivashini";
-		Student2.age=21;
-		Student2.gender="Female";
-		Student2.dayspresent=165;
-
-		System.out.println(Student2.studentname);
-		System.out.println(Student2.age);
-		System.out.println(Student2.gender);
-		System.out.println(Student2.dayspresent);
-       
-}
+	    System.out.println(Student1.gender);
+	    System.out.println(Student1.numofSubjects);
+	   
+	Student Student2=new Student();
+	Student2.studentName="Nivashini";
+	Student2.age=20;
+	Student2.gender="Female";
+	Student2.numofSubjects=5;
+	System.out.println(Student2.studentName);
+	System.out.println(Student2.age);
+	System.out.println(Student2.gender);
+	System.out.println(Student2.numofSubjects);
+	System.out.println("Nivashini punda not used till date");
+	}
 }
