0268201a3c4ec4ba7c4b9f68fd683c41590373c7
NivashiniRamganesh30 <nivashini3093@gmail.com>
2024-11-28 12:26:03 +0530

    Add files via upload
    day7

---
+++ StringConcept.java
@@ -0,0 +1,13 @@
+package day7;
+import java.util.Scanner;
+public class StringConcept {
+	public static void main(String[] args) {
+		Scanner na=new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name="Nivashini";
+		System.out.println(name.substring(0,1));
+		na.close();
+	}
+	
+
+}
