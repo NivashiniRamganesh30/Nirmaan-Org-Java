3f02bd155c5959e9ceec4308914cbcbd7b9992b6
NivashiniRamganesh30 <nivashini3093@gmail.com>
2024-11-28 11:54:45 +0530

    Add files via upload
    day5

---
+++ ForLoopEven.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class ForLoopEven {
+	
+	    public static void main(String[] args) {
+				for (int i=0;i<=100;i++)
+				if (i%2==0) {
+					System.out.println("Even");
+				}
+				else {
+					System.out.println("i");
+				}
+	    }
+	}                                                                                                                                                                                                                                                                                                                                                                       
+
+
+
