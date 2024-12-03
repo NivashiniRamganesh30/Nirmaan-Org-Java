25a9513f23b6d41a9e68bfac9970c2f6f4b9e959
NivashiniRamganesh30 <nivashini3093@gmail.com>
2024-12-03 13:14:07 +0530

    Add files via upload
    DAY8 PART1

---
+++ VaaranamAayiram.java
@@ -0,0 +1,15 @@
+package day8;
+
+public class VaaranamAayiram {
+  public static void main(String[] args) {
+	  boolean isLives = false;
+	if(isLives) {
+		System.out.println("Surya weds Meghna");
+	}
+	else{
+		System.out.println("Surya weds Priya");
+		
+	}
+		
+}
+}
