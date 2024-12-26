a43139be1a8a6ab8b59e80730887360fa442bcef
NivashiniRamganesh30 <nivashini3093@gmail.com>
2024-12-26 13:21:13 +0530

    Add files via upload
    day17

---
+++ Wrapper.java
@@ -0,0 +1,18 @@
+package day17;
+
+public class Wrapper {
+	public static void main(String[] args) {
+	int wrapnum = 10;
+	Integer w = wrapnum;
+	System.out.println(w);
+	
+	int num=20;
+	int AutoBoxingInt=num;
+	System.out.println(AutoBoxingInt);
+	
+	int num1=30;
+	int UnboxingInt=num1;
+	System.out.println(UnboxingInt);
+	}
+
+}
